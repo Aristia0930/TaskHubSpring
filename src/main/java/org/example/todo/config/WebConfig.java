f44f28ebf68eb1051b1f0dbd00a9760cbbdb3130
ari <kjk9858@gmail.com>
2024-12-30 16:49:25 +0900

    주소 다시 추가

--- src/main/java/org/example/todo/config/WebConfig.java
+++ src/main/java/org/example/todo/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000") // React 클라이언트 주소
+                .allowedOrigins("http://localhost:3000","https://localhost:3000") // React 클라이언트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowCredentials(true) // 자격 증명(쿠키) 허용
                 .allowedHeaders("*"); // 모든 헤더 허용

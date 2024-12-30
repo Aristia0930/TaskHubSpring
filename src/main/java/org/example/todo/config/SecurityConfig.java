fcb24c999352c35bdb931047e140e9b609acb7e4
ari <kjk9858@gmail.com>
2024-12-30 19:59:30 +0900

    시큐리티 securityContextRepository

--- src/main/java/org/example/todo/config/SecurityConfig.java
+++ src/main/java/org/example/todo/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 import javax.swing.plaf.PanelUI;
@@ -62,7 +63,9 @@ public class SecurityConfig {
                 .sessionManagement((session) -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
                 })
-
+                .securityContext((context) -> {
+                    context.securityContextRepository(new HttpSessionSecurityContextRepository());
+                })
 
                 .build();
     }

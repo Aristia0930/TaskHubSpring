8dff6d7fc7375047bf7376b1e60970746fc1a2e2
ari <kjk9858@gmail.com>
2024-12-30 20:47:48 +0900

    시큐리티에서 크로스 설정하기

--- src/main/java/org/example/todo/config/SecurityConfig.java
+++ src/main/java/org/example/todo/config/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.swing.plaf.PanelUI;
 
@@ -32,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
-                .cors()
+                .cors().configurationSource(corsConfigurationSource()) // CORS 설정 적용
                 .and()
                 .csrf().disable()
                 .addFilterBefore(new SecurityContextPersistenceFilter(), BasicAuthenticationFilter.class)
@@ -68,7 +71,26 @@ public class SecurityConfig {
                 })
 
                 .build();
+
+
+
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000"); // React 클라이언트 주소
+        configuration.addAllowedOrigin("https://localhost:3000"); // HTTPS 주소 (필요한 경우)
+        configuration.addAllowedOrigin("https://6772552a00bb2638344f7050--takehub.netlify.app"); // Netlify 배포 주소
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메소드 허용 (GET, POST 등)
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 쿠키와 자격 증명 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 적용
+
+        return source;
+    }
+
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        return http
